e6bf635a6514848532f6f773b0dd866480615769
Sione Tenifa <johntenifa@yahoo.com>
2019-03-06 09:32:28 -0700

    second

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ var leftHand = []
 var rightHand = []
 var op_sym = []
 
-document.getElementById("seven").addEventListener("click", seVen);
-function seVen() {
-  leftHand.push(7)
+
+
+var numbers = document.getElementsByClassName("numbers")
+var action = document.getElementsByClassName("action")
+var decimal = document.getElementsByClassName("decimal")
+var equal = document.getElementsByClassName("equal")
+var allClear = document.getElementsByClassName("allClear")
+
+
+function numbers() {
+  for (i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function())
+    debugger
+  }
 }
 
 
 
 
 
+// var blankButt = document.getElementsByClassName('blank')
+// var count = 0
+// var blankButts = false
+
+// function callButton() {
+//     for (i = 0; i < blankButt.length; i++) {
+//         blankButt[i].addEventListener('click', buttPress)
+//     }
+// }
+// callButton();
+
+// function buttPress() {
+//     blankButts = !blankButts
+//     if (blankButts) {
+//         this.children[0].src = './assets/images/x.png';
+//     } else {
+//         this.children[0].src = './assets/images/o.png';
+//     }
+// }
